5b8a173b19c9f64f1234ee2972df6cbce9bc21fb
Raj nayabhari <nayabhariraj97@gmail.com>
2024-06-18 00:33:22 +0545

    deploy

--- database.py
+++ database.py
@@ -16,9 +16,9 @@ def get_db_connection():
         #for render.com
         
         database="cms_7j5a",
-        user="cms_7j5a_user",
-        password="MAFQ8hwWXNIo3JQIC3Zc2gqW8fPGdKhX",
-        host="dpg-cpo6qpo8fa8c739n2ie0-a",
+        user="digi_muni",
+        password="52ODMHwZil8AUVUFP2uOl13ZtiHtJz8j",
+        host="dpg-cpo891bv2p9s73fruoag-a",
         port="5432"
     )
 def database():
